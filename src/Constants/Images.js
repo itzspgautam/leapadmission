6557ce0e2f4e5a11cf8b23a50782a455b7d10e42
unknown <itzspgautam@gmail.com>
2023-02-19 03:26:34 +0530

    Page Load Fix

--- src/Constants/Images.js
+++ src/Constants/Images.js
@@ -2,7 +2,7 @@ export const Images = {
   LOGO_WIDE: require("../Assets/Images/logo.svg"),
   USER_DEFAULT: require("../Assets/Images/guest.png"),
   HRO_BG:
-    "https://res.cloudinary.com/itzspgautam/video/upload/v1675245237/j4mc9agxzzylsbweeoj3.mp4",
+    "https://res.cloudinary.com/dltkycv3q/video/upload/v1676756575/heroBg_1_meyszi.mp4",
   WCU_SUPPORT: require("../Assets/Images/support.png"),
 
   UNIVERSITIES: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
