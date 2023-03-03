f40645d019ba66db3a2c0afc648f273d25e0b5a7
unknown <itzspgautam@gmail.com>
2023-03-03 12:14:56 +0530

    analytics sitemap

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -10,6 +10,16 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap"
           rel="stylesheet"
         />
+
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-E5S71GQYRT"
+        />
+        <script>
+          window.dataLayer = window.dataLayer || []; function gtag()
+          {dataLayer.push(arguments)}
+          gtag('js', new Date()); gtag('config', 'G-E5S71GQYRT');
+        </script>
       </Head>
       <body>
         <Main />
